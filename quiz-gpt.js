967606e590c7af5e04eed63a06c9d1992f2a9985
Saksham Bindal <saksham.bindal2004@gmail.com>
2023-03-30 22:49:53 +0530

    changing dom

--- quiz-gpt.js
+++ quiz-gpt.js
@@ -41,6 +41,34 @@ const askGPT = async (question = "", options = [], ind = 0) => {
   }
 };
 
+const changeAnswers = (type, question, answer) => {
+  try {
+    if (type == 0) {
+      // Short Text
+      question.querySelector("input.whsOnd.zHQkBf").innerHTML = answer;
+      // .setAttribute("data-initial-value", answer);
+    } else if (type == 1) {
+      // Paragraph
+      // question
+      //   .querySelector("textarea.KHxj8b.tL9Q4c")
+      //   .setAttribute("data-initial-value", answer);
+    } else if (type == 2) {
+      // Multiple Choice
+      let options = question
+        .querySelector("div.SG0AAe")
+        .querySelectorAll("div.nWQGrd.zwllIb");
+      let rightOption = Number(answer[8]) - 1;
+      options[rightOption].querySelector("label").click();
+    } else if (type == 3) {
+      // Dropdown
+    } else if (type == 4) {
+      // Checkboxes
+    }
+  } catch (error) {
+    console.error({ ind: i, msg: "Error when changing answer", error });
+  }
+};
+
 async function getSolutions() {
   const questionElements = Array.from(
     document.querySelectorAll("div.Qr7Oae")
@@ -48,32 +76,42 @@ async function getSolutions() {
 
   const solution = [];
 
-  for (let i = 0; i < questionElements.length; i++) {
-    try {
-      const question = questionElements[i];
-      const questionObject = Array.from(
-        JSON.parse("[" + question.getAttribute("data-params").substring(4))
-      )[0];
-      const questionText = questionObject[1];
-      const questionOptions = Array.from(questionObject[4][0][1]).map(
-        (el) => el[0]
-      );
-      const answer = await askGPT(questionText, questionOptions, i + 1);
-      solution.push({ questionText, answer });
+  let promises = [];
 
-      // Changing Answers
-      if (questionObject[3] == 2) {
-        let options = question
-          .querySelector("div.SG0AAe")
-          .querySelectorAll("div.nWQGrd.zwllIb");
-        let rightOption = Number(answer[8]) - 1;
-        options[rightOption].querySelector("label").click();
-      }
-    } catch (error) {
-      console.error({ ind: i, msg: "Error in dom elements", error });
-    }
+  for (let i = 0; i < questionElements.length; i++) {
+    promises.push(
+      new Promise(async (resolve, reject) => {
+        try {
+          const question = questionElements[i];
+          const questionObject = Array.from(
+            JSON.parse("[" + question.getAttribute("data-params").substring(4))
+          )[0];
+          const questionText = questionObject[1];
+          const questionOptions = Array.from(questionObject[4][0][1]).map(
+            (el) => el[0]
+          );
+          const answer = await askGPT(questionText, questionOptions, i + 1);
+          solution.push({ ind: i, questionText, answer });
+
+          // Showing answers in DOM
+          let node = document.createElement("span");
+          node.innerText = answer;
+          question.parentElement.insertBefore(node, question);
+
+          // Changing Answers
+          changeAnswers(questionObject[3], question, answer);
+          resolve();
+        } catch (error) {
+          console.error({ ind: i, msg: "Error in execution", error });
+          reject();
+        }
+      })
+    );
   }
 
+  await Promise.all(promises);
+
+  solution.sort((a, b) => a.ind - b.ind);
   console.log(solution);
 }
 
